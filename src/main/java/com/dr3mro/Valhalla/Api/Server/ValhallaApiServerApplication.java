31f08bb4c9d5bb65579c0406d6b0bb7917f7ee7c
Dr. Amr Osman <dr3mro@gmail.com>
2025-10-05 23:05:17 -0400

    first commit

---
+++ src/main/java/com/dr3mro/Valhalla/Api/Server/ValhallaApiServerApplication.java
@@ -0,0 +1,13 @@
+package com.dr3mro.Valhalla.Api.Server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ValhallaApiServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ValhallaApiServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dr3mro/Valhalla/Api/Server/ValhallaApiServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dr3mro.Valhalla.Api.Server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ValhallaApiServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
